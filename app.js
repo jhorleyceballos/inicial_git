b4f2926800e201829e746c40fc911b78319e6236
Miguel Ricardo Carmona Cantillo <miguelrc1208@gmail.com>
2016-03-03 23:04:10 -0500

    abrir index.html

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-document.write("lo ques sea")
+var x               =   50;
+var y               =   50;
+var canvas          =   null;
+var ctx             =   null;
+var lastPress       =   null;
+function draw (ctx) {
+    ctx.fillStyle   = 'rgb(57, 57, 57)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle   = '#0f0';
+    ctx.fillRect(x, y, 10, 10);
+
+    ctx.fillText(lastPress, 20, 20);
+}
+
+document.addEventListener('keydown', ev => {
+    lastPress       =   ev.which;
+}, false);
+
+function run() {
+    window.requestAnimationFrame(run);
+    update();
+    draw(ctx);
+}
+
+function update(){
+    if (x > canvas.width) x = 0
+    if (x < 0) x = canvas.width
+    if (y > canvas.height) y = 0
+    if (y < 0) y = canvas.height
+    //left
+    if (lastPress === 39) x += 2;
+    //right
+    if (lastPress === 37) x -= 2;
+    //up
+    if (lastPress === 38) y -= 2;
+    //down
+    if (lastPress === 40) y += 2;
+    //default
+    if (lastPress === null) x += 2;
+}
+
+function init() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    run();
+}
+
+window.addEventListener('load', init, false);
